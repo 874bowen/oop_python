64a8f02b648c4ee7ee8ade2ccb300bbb02e7b6b0
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-04-08 11:04:21 +0300

    Initial setup

---
+++ my_money/money.py
@@ -0,0 +1,18 @@
+import sys
+
+
+class Money:
+    pass
+
+
+def test(file):
+    pass
+
+
+def main():
+    input_file = sys.argv[1]
+    test(input_file)
+
+
+if __name__ == "__main__":
+    main()

f3be3a83312c7c5ff1530cc169160f1017f97fd4
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-04-10 18:06:39 +0300

    docstrings added

--- my_money/money.py
+++ my_money/money.py
@@ -2,14 +2,29 @@ import sys
 
 
 class Money:
+    """
+    this class should make an object
+    with attributes equity, debt, gold
+    :param equity, debt, gold:
+    """
     pass
 
 
 def test(file):
+    """
+    this class instantiates an object
+    of class Money
+    :param file:
+    """
     pass
 
 
 def main():
+    """
+    the function that starts our program
+    our command line will take 1 argument
+    :param
+    """
     input_file = sys.argv[1]
     test(input_file)
 

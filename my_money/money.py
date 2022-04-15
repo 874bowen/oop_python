664da9483c0a78c710820adf686a906c1182e87f
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-04-15 18:14:50 +0300

    initialized debt, equity and gold to class Money

--- my_money/money.py
+++ my_money/money.py
@@ -7,7 +7,20 @@ class Money:
     with attributes equity, debt, gold
     :param equity, debt, gold:
     """
-    pass
+    # class attributes (pay_rate)
+    all = []
+
+    def __init__(self, equity: int, debt: int, gold: int):
+        # Run validations to the received args
+        assert debt >= 0, f"debt {equity} is not greater than or equal to zero!"
+        assert gold >= 0, f"gold {debt} is not greater than or equal to zero!"
+        assert gold >= 0, f"gold {gold} is not greater than or equal to zero!"
+
+        # Assign to self object
+        self.equity = equity
+        self.debt = debt
+        self.gold = gold
+        # initial rates
 
 
 def test(file):

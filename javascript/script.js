d1e800c2477a8583a06c28eb0218db1217e90ce8
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-04-16 20:49:08 +0300

    Object Oriented Programming in JS wooow!!

--- javascript/script.js
+++ javascript/script.js
@@ -7,3 +7,25 @@ function product(a, b) {
     return val
 }
 console.log("9 * 6 = " + product(9, 6) + "\nglobal = " + gl)
+
+// class PartyAnimal with name as a parameter constructor
+function PartyAnimal(nam){
+    // each object created has some data(variables)
+    this.x = 0;
+    this.name = nam;
+    console.log("I am constructed");
+    // each object created has some code(function)
+    this.party = function() { // function party
+        this.x = this.x + 1;
+        console.log("x is " + this.x);
+    }
+}
+// create an instance of class PartyAnimal
+an = new PartyAnimal("Jim");
+// call function party() on object an
+an.party();
+an.party();
+an.party();
+dog = new PartyAnimal("Simba");
+dog.party();
+dog.party();

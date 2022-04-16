300fc7eeea511c40c4742b9bbe23dfc9caae56b4
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-04-16 20:11:36 +0300

    to make variable in js local we add 'var' keyword

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,9 @@
 document.write("<p>I am called from outside</p>");
-
+gl = 123
 function product(a, b) {
+    // to make variable in js local we add 'var'
+    var gl = 456
     val = a*b;
     return val
 }
-console.log("9 * 6 = " + product(9, 6))
+console.log("9 * 6 = " + product(9, 6) + "\nglobal = " + gl)

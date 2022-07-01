152ec257234cea8fd62e72635345ac4d4a0c2b38
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-07-01 22:08:16 +0300

    bubblesort sorting algorithm

---
+++ Design and Analysis of Algorithms/bubblesort.py
@@ -0,0 +1,20 @@
+from swap import swap
+
+""":param
+have a list
+check if first element is greater than the next
+if true swap
+"""
+
+
+def bubblesort(lst, lb, ub):
+    for j in range(len(lst)-1):
+        print(j)
+        for i in range(len(lst)-1):
+            if lst[i] > lst[i+1]:
+                print("Greater")
+                print(swap(lst, i, i+1))
+
+
+bubblesort([25, 3, 14, 6, 23], 0, 4)
+

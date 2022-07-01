e6cbd450228bff67b53bf900f7ff8530e77db0f7
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-07-01 21:33:24 +0300

    partition function to partition the list

--- Design and Analysis of Algorithms/quicksort.py
+++ Design and Analysis of Algorithms/quicksort.py
@@ -3,3 +3,23 @@ def swap(lst, pos1, pos2):
     return lst
 
 
+def partition(lst, lb, ub):
+    pivot = lst[0]
+    start = lb
+    end = ub
+
+    while start < end:
+        while lst[start] <= pivot:
+            start = start + 1
+            # print("start", start)
+            break
+        while lst[end] > pivot:
+            end = end - 1
+            # print("end", end)
+            break
+        if start < end:
+            swap(lst, start, end)
+
+        swap(lst, lst.index(pivot), end)
+    return end
+
